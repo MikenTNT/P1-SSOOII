d3b999134b9f4fa570b797d87c589c49eec2970e
MikenTNT <miguel.sanchezgon@gmail.com>
2019-03-19 09:28:18 +0100

    v0.1 B(OK)

--- falonso.c
+++ falonso.c
@@ -1,112 +1,152 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/shm.h>
 
-#include <unistd.h>
-#include <signal.h>
+#include <sys/ipc.h>  /* Funcion ftok().  */
+#include <sys/sem.h>  /* Semáforos.  */
+#include <sys/shm.h>
+#include <sys/types.h>  /* Types pids.  */
+
+#include <signal.h>  /* Tratamiento de señales.  */
+#include <unistd.h>  /* Librería para fork().  */
 
 #include "falonso.h"
 
-#define TAM 304
-
-int inicio_falonso(int ret, int semAforos, char *z);
-
-void doAll(int semaforo, int nSemaforo, int nSignal);
-//void salir();
-
-// struct semid_ds
-// {
-// 	int sem_nsems;
-//     time_t sem_otime;
-//     time_t sem_ctime;
-// };
-
+/* Variables globales.  */
 union semun {
-	int             val;
+	int val;
 	struct semid_ds *buf;
-	ushort_t        *array;
-};
+	ushort_t *array;
+} s;
 
 
+int main(int argc, char const *argv[]) {
 
+	// if (signal(SIGINT, salir) == SIG_ERR) {
+	// 	perror("No se puede capturar la señal.");
+	// 	exit(ERR_CAP_SIGN);
+	// }
 
-int main(void) {
-
-	int memoriaID, s1;
+	int memoriaID, semaforo;
 	char *puntero = NULL;
+	pid_t pidPadre, pidHijo;
+	int desp = 4;
+	int carril = 0;
 
+	/* Crea y calcula una clave para la función semget().  */
+	key_t clave = ftok(argv[0], 'M');  /* IPC_PRIVATE.  */
 
-
-	//signal(SIGINT, salir);
-
-	//Creamos los semaforos
-	if (-1 == (s1 = semget(IPC_PRIVATE, 2, IPC_CREAT | 0600))) {
-		printf("No se pudo crear el semaforo");
-		return -2;
+	/* Inicia y comprueba la creacíon de un semáforo.  */
+	if (-1 == (semaforo = semget(clave, 2, IPC_CREAT | 0600))) {
+		perror("No se pudo crear el semaforo");
+		exit(ERR_CR_SEM);
 	}
+
 	struct semid_ds ssd;
-	ssd.sem_nsems = 2;
-	//Los inicializamos
-	semctl(s1, 0, IPC_STAT, &ssd);
-	semctl(s1, 1, IPC_STAT, &ssd);
+	//struct shmid_ds sds;
+	ssd.sem_nsems = 2 ;
+	s.buf = &ssd;
 
-	// Creamos la memoria compartida
+	/* Damos valor a los semáforos.  */
+	semctl(semaforo, 0, IPC_STAT, s.buf);
+	semctl(semaforo, 1, IPC_STAT, s.buf);
+
+	/* Inicia y comprueba la creacíon de memoria compartida.  */
 	puts("Vamos a crear la memoria compartida");
-	if((memoriaID = shmget(IPC_PRIVATE, TAM, 0666| IPC_CREAT)) == -1){
-		printf("No se ha podido crear la memoria\n");
+	if (-1 == (memoriaID = shmget(IPC_PRIVATE, TAM, IPC_CREAT | 0666))) {
+		perror("No se ha podido crear la memoria");
+		exit(ERR_CR_MEM);
 	}
-	//Asociación del puntero a la memoria compartida
+
+	/* Asociación del puntero a la memoria compartida.  */
 	puntero = shmat(memoriaID, NULL, 0);
 
-	inicio_falonso(1, s1, puntero);
 
-
-	//write(1, "msg", strlen[4]);
-	return 1;
-}
-
-
-//Función que nos permite hacer signal y wait
-void doAll(int semaforo, int nSemaforo, int nSignal){
-
-	struct sembuf pedir;
-
-	pedir.sem_num = nSemaforo;
-	pedir.sem_op = nSignal;
-	pedir.sem_flg = 0;
-
-	if(semop(semaforo, &pedir, 1) == -1){
-		perror(NULL);
-		perror("Error al hacer Signal\n");
+	pidPadre = getpid();
+	switch (fork()) {
+		case -1:
+			perror("Error de creacion del hijo");
+			exit(ERR_CR_HIJO);
+		case 0:
+			pidHijo = getpid();
+			inicio_coche(&carril, &desp, 1);
+			// puntero = shmat(memoriaID, NULL, 0); //Asociarse
+			// shmctl(memoriaID, IPC_STAT, )
+			// sprintf(puntero, "%d", 1);
+			puts("Me he creado");
+			break;
 	}
 
+	if (pidPadre == getpid()) {
+		inicio_falonso(1, semaforo, puntero);
+
+		while (1) {
+			luz_semAforo(1,1);
+			luz_semAforo(0,2);
+			sleep(1);
+			luz_semAforo(1,2);
+			luz_semAforo(0,1);
+			sleep(1);
+		}
+	}
+
+	if (pidHijo == getpid()) {
+		while (1) {
+			avance_coche(&carril, &desp, 1);
+		}
+	}
+
+	return 1;
+
 }
 
-// void salir(){
 
-// 	signal(SIGINT,SIG_IGN);
-// 	int i, data;
+/* Función para hacer signal y wait.  */
+void opSemaforo(int semaforo, int nSemaforo, int nSignal)
+{
+	/* Declaramos la estructura de control del semáforo.  */
+	struct sembuf opSem;
 
-// 	// for (i = 0; i < 5; i++) {
+	/* Establecemos los valores de control.  */
+	opSem.sem_num = nSemaforo;
+	opSem.sem_op = nSignal;
+	opSem.sem_flg = 0;
+
+	/* Ejecutamos la operación del semáforo.  */
+	if (-1 == semop(semaforo, &opSem, 1)) {
+		perror("Error al hacer Signal\n");
+		exit(ERR_OP_SEM);
+	}
+}
+
+
+/* Función de captura de la señal CTRL+C.  */
+// void salir()
+// {
+// 	/* Ignoramos las nuevas señales.  */
+// 	signal(SIGINT, SIG_IGN);
+
+// 	int data;
+
+// 	/* Eliminamos los hijos.  */
+// 	// for (int i = 0; i < 5; i++) {
 // 	// 	kill(hijos[i], SIGINT);
 // 	// }
 
 // 	shmdt(&puntero);
-// 	if((shmctl(memoriaID, IPC_RMID, NULL))==-1){
-// 		printf("Problema al liberar la memoria\n");
+// 	if (shmctl(memoriaID, IPC_RMID, NULL) == -1) {
+// 		perror("Problema al liberar la memoria");
 // 	}
 
 // 	if (semctl(s1, 0, IPC_RMID) == -1) {
-// 		perror("Error en borrado de semáforo");
-// 		exit(5);
+// 		perror("Error en el borrado de los semáforos");
+// 		exit(ERR_RM_SEM);
 // 	}
 
-// 	system("clear"); /* Bueno para borrar huellas... */
-// 	signal(SIGINT, salir);
-// 	exit(0);
+// 	// system("clear"); /* Bueno para borrar huellas... */
 
+// 	/* Activamos de nuevo las señales.  */
+// 	signal(SIGINT, salir);
+
+// 	exit(0);
 // }
